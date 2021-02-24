af050978a5b2de3d19a3514ab1da219d2d7b962c
Pavan Jain <013pavanjain@gmail.com>
2021-02-24 23:26:32 +0530

    First Commit

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Country, Feedback, Season, Wonder
+
+admin.site.register(Country)
+admin.site.register(Feedback)
+admin.site.register(Season)
+admin.site.register(Wonder)
